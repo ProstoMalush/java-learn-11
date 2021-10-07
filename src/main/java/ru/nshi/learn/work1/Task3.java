44a3dbbed47321f6f3af45113d4c5f4155f7ff01
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-07 21:36:20 +0400

    work1 Task3 fixed.

--- src/main/java/ru/nshi/learn/work1/Task3.java
+++ src/main/java/ru/nshi/learn/work1/Task3.java
@@ -1,20 +1,26 @@
 package ru.nshi.learn.work1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
         Task3 task3 = new Task3();
-        task3.PrimeNumber(17);
+        System.out.print("введите число: ");
+        Scanner in = new Scanner(System.in);
+        int inputValue = in.nextInt();
+        System.out.println(task3.PrimeNumber(inputValue));
+
     }
-    public static void PrimeNumber(int number){
-        int counter = 0;
+    public boolean PrimeNumber(int number){
+        int result = 0;
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
-                counter++;
+                result++;
             }
         }
-        if (counter == 2) {
-            System.out.println("простое число");
-        } else
-            System.out.println("не простое число");
+        if (result == 2) {
+            return true;
+        } else return false;
     }
 }
