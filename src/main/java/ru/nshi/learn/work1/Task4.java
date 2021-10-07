118955dc0b5066e01bebad465d1c731788351811
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-07 21:36:27 +0400

    work1 Task4 fixed.

--- src/main/java/ru/nshi/learn/work1/Task4.java
+++ src/main/java/ru/nshi/learn/work1/Task4.java
@@ -1,24 +1,23 @@
 package ru.nshi.learn.work1;
-
+//Написать программу, которая определяет сумму всех простых чисел в диапазоне от 1000 до 2000.
 public class Task4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Task4 task4 = new Task4();
-        task4.SummaSimpleNumbers();
+        System.out.println(task4.SumAllPrimes(1000, 2000));
     }
-    public static void SummaSimpleNumbers() {
-        int summa = 0;
-        for (int i = 1000; i <= 2000; i++) {
-            int counter = 0;
-            for (int j = 1; j <= i; j++){
-                if (i % j == 0) {
-                    counter += 1;
-                }
+
+
+
+    public int SumAllPrimes(int start, int end) {
+        Task3 task3 = new Task3();
+        int result = 0;
+        for (int number = start; number <= end; number++) {
+            if (task3.PrimeNumber(number)) {
+                result += number;
             }
-            if (counter == 2) {
-                summa += i;
-            } else
-                continue;
         }
-        System.out.println("сумма простых чисел :" + summa);
+        return result;
     }
 }
+
+
