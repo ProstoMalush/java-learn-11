fcbf17b0f178c5cc14fad92027dd0665a681a1ac
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-07 21:36:09 +0400

    work1 Task2 fixed.

--- src/main/java/ru/nshi/learn/work1/Task2.java
+++ src/main/java/ru/nshi/learn/work1/Task2.java
@@ -3,30 +3,17 @@ package ru.nshi.learn.work1;
 public class Task2 {
     public static void main(String[] args) {
         Task2 task2 = new Task2();
-        task2.SummaAllLuckyNumbers(100000, 999999);
+        System.out.println(task2.SumAllLuckyNumbers(100000, 999999));
     }
-    public static void SummaAllLuckyNumbers(int start, int end) {
-        int counter = 0; int summa = 0;
-        for (int number = start; start <= end; start++) {
-            number = start;
-            int left = 0;
-            int right = 0;
-
-            for (int j = 0; j < 3; j++) {
-                right += number % 10;
-                number /= 10;
-            }
-
-            for (int j = 0; j < 3; j++) {
-                left += number % 10;
-                number /= 10;
-            }
-
-            if (left == right) {
-                counter++;
+    public int SumAllLuckyNumbers(int start, int end) {
+        Task1 task1 = new Task1();
+        int result = 0;
+        for (int number = start; number <= end; number++) {
+            if (task1.LuckyNumber(number)) {
+                result += number;
             }
         }
-
-        System.out.println(counter);
+        return result;
     }
+
 }
