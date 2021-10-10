aae453450afb1d2293722030ac4a183773abdfda
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-10 16:25:54 +0400

    work2 Task2 fixed.

--- src/main/java/ru/nshi/learn/work2/Task2.java
+++ src/main/java/ru/nshi/learn/work2/Task2.java
@@ -1,47 +1,33 @@
 package ru.nshi.learn.work2;
-
+//Найти сумму положительных элементов одномерного массива. Диапазон случайных значений [-50, 51).
 import java.util.Random;
 
 public class Task2 {
     public static void main(String[] args) {
         Task2 task2 = new Task2();
-        int[] array = task2.createArray(10);
-        array = task2.fillArrayByRandom(array);
-        int Summa = task2.SummaArray(array);
-        task2.printArray(array);
-        System.out.println("\nSumma is " + Summa);
-    }
+        Task1 task1 = new Task1();
 
-    public int[] createArray(int length) {
-        int[] array = new int[length];
-        return array;
+        int[] array = task1.createArray(10);
+        array = task2.fillArrayByRandom(array);
+        task1.printArray(array);
+        System.out.println("\nсумма положительных = " + task2.SumArray(array));
     }
 
     public int[] fillArrayByRandom(int[] array) {
         Random rnd = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = rnd.nextInt(101) - 50 ;
+            array[i] = rnd.nextInt(101)-50;
         }
         return array;
     }
 
-    public void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i + 1 < array.length) {
-                System.out.print(", ");
-            }
-        }
-    }
-
-    public int SummaArray(int[] array) {
-        int Summa=0;
+    public int SumArray(int[] array) {
+        int Sum=0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 0) {
-                Summa += array[i];
+                Sum += array[i];
             }
-
         }
-        return Summa;
+        return Sum;
     }
 }
