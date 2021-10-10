6960e78df910c1b9532cf357a4806c7d9987243f
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-10 16:26:46 +0400

    work2 Task4 fixed.

--- src/main/java/ru/nshi/learn/work2/Task4.java
+++ src/main/java/ru/nshi/learn/work2/Task4.java
@@ -1,6 +1,8 @@
 //Поменять местами минимальный и максимальный элементы одномерного массива.
 package ru.nshi.learn.work2;
 
+
+
 public class Task4 {
 
     public static void main(String[] args){
@@ -8,96 +10,66 @@ public class Task4 {
         Task3 task3 = new Task3();
         Task1 task1 = new Task1();
 
-
-
-        int[] array = task1.createArray(5);
+        int[] array = task1.createArray(10);
         task1.fillArrayByRandom(array);
+        System.out.println("начальный массив");
         task1.printArray(array);
+        System.out.println("\nконечный массив");
+        task4.printArray4(array);
+
+        System.out.println();
 
         System.out.println("\nМаксимальное значение массива = " + task3.maxValue(array));
         System.out.println("Минимальное значение массива = " + task3.minValue(array));
-        System.out.println("индекс максимального значения массива = " + task4.IndexMax1(array));
-        System.out.println("индекс минимального значения массива = " + task4.IndexMin1(array));
-        task4.printArray4(array);
-
+        System.out.println("индекс максимального значения массива = " + task4.IndexMax(array));
+        System.out.println("индекс минимального значения массива = " + task4.IndexMin(array));
 
     }
 
-
-
-
-    public int maxValue(int[] array) {
+    public int IndexMax(int[] array) {
         int max = array[0];
-        int max1;
-        for (int i = 0; i < array.length; i++) {
-            if (max > array[i]){
-                max = array[i];
-                max1 = i;
-            }
-        }
-        return max;
-    }
-
-    public int IndexMax1(int[] array) {
-        int max = array[0];
-        int max1 = 0;
+        int Imax = 0;
         for (int i = 0; i < array.length; i++) {
             if (max < array[i]){
                 max = array[i];
-                max1 = i;
+                Imax = i;
             }
         }
-        return max1;
+        return Imax;
     }
 
-    public int minValue(int[] array){
+    public int IndexMin(int[] array) {
         int min = array[0];
-        int min1;
-        for (int i = 1; i < array.length; i++)
-        {
-            if (min < array[i]){
-                min = array[i];
-                min1 = i;
-            }
-        }
-        return min;
-    }
-
-    public int IndexMin1(int[] array) {
-        int min = array[0];
-        int min1 = 0;
+        int Imin = 0;
         for (int i = 0; i < array.length; i++) {
             if (min > array[i]){
                 min = array[i];
-                min1 = i;
+                Imin = i;
 
             }
         }
-        return min1;
+        return Imin;
     }
 
-
-
     private void printArray4(int[] array) {
+
         Task4 task4 = new Task4();
-        int min1 = task4.IndexMin1(array);
-        int max1 = task4.IndexMax1(array);
-        int min = task4.minValue(array);
-        int max = task4.maxValue(array);
+        Task3 task3 = new Task3();
+        int Imin = task4.IndexMin(array);
+        int Imax = task4.IndexMax(array);
+        int min = task3.minValue(array);
+        int max = task3.maxValue(array);
+
         for (int i = 0; i < array.length; i++) {
-            if (i != min1 && i != max1){
+            if (i == Imax){
+                System.out.print(min + ", ");
+            } else if (i == Imin){
+                System.out.print(max + ", ");
+            } else {
                 System.out.print(array[i]);
-            }
-            if (i == min1){
-                array[i] = max;
-                System.out.print(array[max1]);
-            }
-            if (i == max1){
-                array[i] = min;
-                System.out.print(array[min1]);
-            }
-            if (i + 1 < array.length) {
-                System.out.print(", ");
+                if (i + 1 < array.length) {
+                    System.out.print(", ");
+                }
             }
         }
     }
