687bee8bb6a203f37590928291674d016ff6bda3
ProstoMalush <90964508+ProstoMalush@users.noreply.github.com>
2021-10-10 16:23:52 +0400

    work2 Task1 fixed.

--- src/main/java/ru/nshi/learn/work2/Task1.java
+++ src/main/java/ru/nshi/learn/work2/Task1.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Task1 {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
-        int[] array = task1.createArray(10);
+        int[] array = task1.createArray(5);
         array = task1.fillArrayByRandom(array);
         task1.printArray(array);
     }
@@ -18,7 +18,7 @@ public class Task1 {
     public int[] fillArrayByRandom(int[] array) {
         Random rnd = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = rnd.nextInt(9)+1;
+            array[i] = rnd.nextInt(90)+10;
         }
         return array;
     }
