ba6b38d27fd507639beb8e1638cc415847d0c89a
Max <90964508+ProstoMalush@users.noreply.github.com>
2021-11-08 18:00:22 +0400

    work3 task3 fixed code.

--- src/main/java/ru/nshi/learn/work3/Task3.java
+++ src/main/java/ru/nshi/learn/work3/Task3.java
@@ -1,6 +1,6 @@
 package ru.nshi.learn.work3;
 
-//ЗАДАНИЕ 3: Осуществить проверку двумерного массива на "магический квадрат".
+// Осуществить проверку двумерного массива на "магический квадрат".
 
 public class Task3 {
     public static void main(String[] args){
@@ -10,11 +10,13 @@ public class Task3 {
         int[][] array = task1.createMatrix();
         task1.randomFillingMatrix(array);
         task1.printMatrix(array);
-
+        task1.sumLinesArray(array);
         System.out.println();
         System.out.println("Сумма элементов главной диагонали = " + task3.sumMainDiagonal(array));
         System.out.println("Сумма элементов побочной диагонали = " + task3.sumCollateralDiagonal(array));
-        task3.magicMatrix(task3.sumMainDiagonal(array), task3.sumCollateralDiagonal(array));
+        if(task3.magicMatrix(array)) System.out.println("квадрат магический");
+            else System.out.println("квадрат не магический");
+
     }
 
     public int sumMainDiagonal (int[][] array){
@@ -41,12 +43,23 @@ public class Task3 {
         return sumCollateral;
     }
 
-    public void magicMatrix(int sumMain, int SumCollateral){
-        boolean flag = false;
-        if (sumMain == SumCollateral){
-            flag = true;
-            System.out.println("Матрица является магической!");
-        } else
-            System.out.println("Матрица не является магической!");
+    public boolean magicMatrix(int[][] array){
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        Task3 task3 = new Task3();
+        int sumMain = task3.sumMainDiagonal(array);
+        int sumCollateral = task3.sumCollateralDiagonal(array);
+        int[] sumLines = task1.sumLinesArray(array);
+        int[] sumColumns = task2.sumColumnsArray(array);
+        task2.sumColumnsArray(array);
+
+        if (sumMain == sumCollateral) {
+            for (int i = 1; i < array.length; i++)
+                {
+                    if (sumLines[i] != sumLines[i-1]) return false;
+                    if (sumColumns[i] != sumColumns[i-1]) return false;
+                }
+        }else return false;
+        return true;
     }
 }
