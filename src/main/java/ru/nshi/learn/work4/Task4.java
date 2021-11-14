ca5ffb05c569258fc23515c9ee89f42aa446bc17
Max <90964508+ProstoMalush@users.noreply.github.com>
2021-11-14 21:32:20 +0400

    work4 task4 fixed code.

--- src/main/java/ru/nshi/learn/work4/Task4.java
+++ src/main/java/ru/nshi/learn/work4/Task4.java
@@ -9,18 +9,18 @@ public class Task4 {
         String str = "a1b23c4%&50";
         String readyStr = task3.creatingStringWithNumbers(str);
         System.out.println("string = "+readyStr);
-        char[] array = new char[str.length()];
         int[] arr = task3.creatingArrayWithNumbers(readyStr);
         task3.printArray(arr);
         task4.summArrryNumbers(arr);
+        System.out.println("\nsumm = " + task4.summArrryNumbers(arr));
     }
 
-    private void summArrryNumbers(int[] arr) {
+    private int summArrryNumbers(int[] arr) {
         int summ=0;
         Task3 task3 = new Task3();
         for (int i=0;i< arr.length; i++)
             summ += arr[i];
-        System.out.println("\nsumm = " + summ);
+       return summ;
     }
 
 
